98b9c987ef11cd9b9384a5dea1ac86b4b7718259
Rashkevich <50698709+Rashkevich@users.noreply.github.com>
2019-09-21 12:22:11 +0300

    sysout in dbemulation
    sysout

--- tomapp/src/main/java/my/tomcat/app/DBEmul/Testing.java
+++ tomapp/src/main/java/my/tomcat/app/DBEmul/Testing.java
@@ -3,6 +3,7 @@ package my.tomcat.app.DBEmul;
 public class Testing {
     public static void main(String[] a){
         DBemulation db=new DBemulation();
+        System.out.println("data base emulation:");
         System.out.println(db.getLogin()+" "+db.getPassword());
     }
 }
